a6bbdfe4e5cf36f596f6d72ac68f6907aeebcc03
Tomasz Gil <tomaszgil.dev@gmail.com>
2019-12-10 22:19:47 +0100

    Use first few charts

--- dashboard/server.R
+++ dashboard/server.R
@@ -5,13 +5,11 @@ library("Hmisc")
 library(shinydashboard)
 source('data.R', local = TRUE)
 
-
-
 server <- function(input, output) {
   set.seed(122)
   data <- getData()
   
-  print(data)
+  # plot 1 ----------------------------------------------------------------------------------------
   
   data %>%
     group_by(main_category) %>%
@@ -27,9 +25,6 @@ server <- function(input, output) {
     summarise(count = n()) %>%
     mutate(percentage = count / sum(count), label = scales::percent(percentage))
   
-  
-  
-  
   output$plot1 <- renderPlot({
     ggplot(plot1, aes(x = factor(main_category, levels = levels(plot1$main_category)), 
                       y = percentage,
@@ -39,6 +34,84 @@ server <- function(input, output) {
       geom_text(aes(label = label), size = 3, position = position_stack(vjust = 0.5)) +
       scale_fill_manual(values = c("successful" = "green", "failed" = "red", "canceled" = "blue")) +
       labs(y = "Percent", fill = "State", x = "Category", title = "Project state by category") +
+      theme(axis.text.x = element_text(angle = 290, hjust = 0, vjust = 0.5))
+  })
+  
+  # plot 2 ----------------------------------------------------------------------------------------
+  
+  data %>%
+    mutate(year = as.integer(format(launched, "%Y"))) %>%
+    group_by(year) %>%
+    summarise(sucessfull = sum(state == "successful"),
+              failed = sum(state == "failed"),
+              canceled = sum(state == "canceled"),
+              success_rate = round(sum(state == "successful") / n(), digits = 2),
+              failed_rate = round(sum(state == "failed") / n(), digits = 2),
+              canceled_rate = round(sum(state == "canceled") / n(), digits = 2)) %>%
+    arrange(year)
+  
+  plot2 <- data %>%
+    mutate(year = as.integer(format(launched, "%Y"))) %>%
+    group_by(year, state) %>%
+    summarise(count = n()) %>%
+    mutate(percentage = count / sum(count), label = scales::percent(percentage)) %>%
+    arrange(year)
+  
+  output$plot2 <- renderPlot({
+    ggplot(plot2, aes(x = year, y = percentage)) +
+      geom_line(aes(color = state)) + 
+      scale_color_manual(values = c("successful" = "green", "failed" = "red", "canceled"="blue")) + 
+      scale_y_continuous(breaks = seq(0, 1, 0.2), labels = paste(seq(0, 100, 20), "%", sep = "")) +
+      labs(y = "Percent", fill = "State", x = "Year", title = "Project state by year") + 
+      theme_minimal()
+  })
+  
+  # plot 3 ----------------------------------------------------------------------------------------
+    
+  data %>%
+    mutate(month = as.integer(format(launched, "%m"))) %>%
+    group_by(month) %>%
+    summarise(sucessfull = sum(state == "successful"),
+              failed = sum(state == "failed"),
+              canceled = sum(state == "canceled"),
+              success_rate = round(sum(state == "successful") / n(), digits = 2),
+              failed_rate = round(sum(state == "failed") / n(), digits = 2),
+              canceled_rate = round(sum(state == "canceled") / n(), digits = 2)) %>%
+    arrange(month)
+
+  plot3 <- data %>%
+    mutate(month = as.integer(format(launched, "%m"))) %>%
+    group_by(month, state) %>%
+    summarise(count = n()) %>%
+    mutate(percentage = count / sum(count), label = scales::percent(percentage)) %>%
+    arrange(month)
+  
+  output$plot3 <- renderPlot({
+    ggplot(plot3, aes(x = month, y = percentage)) +
+      geom_line(aes(color = state)) + 
+      scale_color_manual(values = c("successful" = "green", "failed" = "red", "canceled" = "blue")) + 
+      scale_y_continuous(breaks = seq(0, 1, 0.2), labels = paste(seq(0, 100, 20), "%", sep = "")) +
+      labs(y = "Percent", fill = "State", x = "Month", title = "Project state by month") + 
+      theme_minimal()
+  })
+  
+  # plot 4 ----------------------------------------------------------------------------------------
+  
+  output$plot4 <- renderPlot({
+    ggplot(data, aes(x = main_category, y = goal, fill = state)) +
+      geom_boxplot() + 
+      labs(fill = "Goal") + 
+      labs(y = "goal", fill = "state", x = "category", title = "Goal amount by category and state") + 
+      theme(axis.text.x = element_text(angle = 290, hjust = 0, vjust = 0.5))
+  })
+  
+  # plot 5 ----------------------------------------------------------------------------------------
+
+  output$plot5 <- renderPlot({
+    ggplot(data, aes(x = main_category, y = pledged, fill = state)) +
+      geom_boxplot() + 
+      labs(fill = "Pledged") + 
+      labs(y = "pledged", fill = "state", x = "category", title = "Pledged amount by category and state") + 
       theme(axis.text.x = element_text(angle = 290, hjust = 0, vjust = 0.5))
   })
 }
--- dashboard/ui.R
+++ dashboard/ui.R
@@ -4,22 +4,43 @@ ui <- dashboardPage(
   dashboardHeader(),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+      menuItem("Plot 1", tabName = "t1", icon = icon("th")),
+      menuItem("Plot 2", tabName = "t2", icon = icon("th")),
+      menuItem("Plot 3", tabName = "t3", icon = icon("th")),
+      menuItem("Plot 4", tabName = "t4", icon = icon("th")),
+      menuItem("Plot 5", tabName = "t5", icon = icon("th")),
+      menuItem("Raw data", tabName = "raw", icon = icon("data"))
     ),
     sliderInput("slider", "Number of observations:", 1, 100, 50)
   ),
   dashboardBody(
     tabItems(
-      # First tab content
-      tabItem(tabName = "dashboard",
+      tabItem(tabName = "t1",
               fluidRow(
-                box(plotOutput("plot1", height = 250))
+                box(plotOutput("plot1", height = 450))
               )
       ),
-      
-      # Second tab content
-      tabItem(tabName = "widgets",
+      tabItem(tabName = "t2",
+              fluidRow(
+                box(plotOutput("plot2", height = 450))
+              )
+      ),
+      tabItem(tabName = "t3",
+              fluidRow(
+                box(plotOutput("plot3", height = 450))
+              )
+      ),
+      tabItem(tabName = "t4",
+              fluidRow(
+                box(plotOutput("plot4", height = 450))
+              )
+      ),
+      tabItem(tabName = "t5",
+              fluidRow(
+                box(plotOutput("plot5", height = 450))
+              )
+      ),
+      tabItem(tabName = "raw",
               h2("Widgets tab content")
       )
     )
